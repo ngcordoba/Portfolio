59b1ae783eebcb9e2b26e8d6669663f4ed7721a6
Gabriel Cordoba <gabrielncordoba@gmail.com>
2022-08-10 14:56:55 -0300

    corrections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { Heading, VStack, Flex, Spacer } from '@chakra-ui/layout'
+import { Heading, VStack, Flex, Spacer, } from '@chakra-ui/react'
+import { useColorModeValue } from '@chakra-ui/color-mode'
 import { useColorMode } from '@chakra-ui/color-mode'
 import { IconButton } from '@chakra-ui/button'
 import { FaSun, FaMoon } from 'react-icons/fa'
 import Body from "./components/Body"
-import Social from "./components/Social"
-import Profile from "./components/Profile"
 
 function App() {
 
@@ -16,16 +15,17 @@ function App() {
     <VStack p="5">
       <Flex w="100%">
         <Heading
-           ml="8" mt="3" size="md" fontWeight= 'semibold'  color="white.500"> ngcordoba </Heading>   
+           ml="2" mt="3" size="md" fontWeight= 'semibold'  color="white.500"> ngcordoba </Heading>   
         <Spacer></Spacer>
-        <IconButton ml={2} size="lg" icon={isDark ? <FaSun/> : <FaMoon/>} onClick={toggleColorMode}></IconButton>
-      </Flex>
+        <IconButton 
+        ml={2} 
+        size="lg" 
+        icon={isDark ? <FaMoon/> : <FaSun/>} 
+        onClick={toggleColorMode}> 
+        </IconButton>
 
-        
+      </Flex>
         <Body></Body>
-        <Social></Social>
-        <Profile></Profile>
-      
     </VStack>
     
   );
--- src/components/Body.js
+++ src/components/Body.js
@@ -21,24 +21,27 @@ function Body() {
       <Flex direction={isNotSmallerScreen ? "row" : "column"}
           spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
           alignSelf="flex-start">
-          <Box mt={isNotSmallerScreen ? "0" : 16} aling='flex-start'>
+          <Box mt={isNotSmallerScreen ? "0" : 16} aling='flex-start' ml="2">
             <Text fontSize="30" fontWeight="semibold">Hi, I am</Text>
             <Text fontSize="50" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip="text">Gabriel Cordoba</Text>
             <Text mb="5" color={isDark ? "gray.200" : "gray.600"}>I'm Functional Analyst and Front End Developer and i'm currently learning ReactJS/Native, 
         developing projects to improve my skills</Text>
-
-        <IconButton size="lg" icon={<FaLinkedin/> } onClick={() => window.open("https://www.linkedin.com/in/ngabrielcordoba/")}> Github </IconButton>
-        <IconButton ml={2} size="lg" icon={<FaGithub></FaGithub>}onClick={() => window.open("https://github.com/ngcordoba")}></IconButton>
-        <IconButton ml={2} size="lg" icon={<FaInstagram></FaInstagram>}onClick={() => window.open("https://www.instagram.com/gabocordoba/")}></IconButton>
+          </Box>
         
-        
-          </Box> 
-          
           <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"}
-                  mb={isNotSmallerScreen ? "0" : "12"} borderRadius='full'
-                  backgroundColor="transparent" boxShadow="lg"
-                  boxSize="300px" src='https://avatars.githubusercontent.com/u/70921504?v=4' />
-              
+                  mb={isNotSmallerScreen ? "0" : "12"} 
+                  borderRadius='full'
+                  backgroundColor="transparent" 
+                  boxShadow="lg"
+                  boxSize="300px" 
+                  src='https://avatars.githubusercontent.com/u/70921504?v=4' 
+                  alt="Profile image"/>
+                  
+          <Box mt={isNotSmallerScreen ? "0" : 16} alignSelf="center">
+            <IconButton size="lg" icon={<FaLinkedin/> } onClick={() => window.open("https://www.linkedin.com/in/ngabrielcordoba/")}> Github </IconButton>
+            <IconButton ml={2} size="lg" icon={<FaGithub></FaGithub>}onClick={() => window.open("https://github.com/ngcordoba")}></IconButton>
+            <IconButton ml={2} size="lg" icon={<FaInstagram></FaInstagram>}onClick={() => window.open("https://www.instagram.com/gabocordoba/")}></IconButton>
+          </Box>
       </Flex>
 
     
